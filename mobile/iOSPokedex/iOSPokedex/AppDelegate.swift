5bf0addeb6ec6d57ce45d450e6d2dd314951b63b
Thiago Oliveira <thiago@outlook.com.br>
2021-01-28 20:50:46 -0300

    AppDelegate config to Home presentation

--- mobile/iOSPokedex/iOSPokedex/AppDelegate.swift
+++ mobile/iOSPokedex/iOSPokedex/AppDelegate.swift
@@ -10,8 +10,18 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        let homeViewController = HomeViewController()
+
+        window?.rootViewController = homeViewController
+        window?.makeKeyAndVisible()
+
         return true
     }
 }
