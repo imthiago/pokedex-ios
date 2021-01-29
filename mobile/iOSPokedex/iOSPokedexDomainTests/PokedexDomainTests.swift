0221846256187e68d294530228c95c34726bf699
Thiago Oliveira <thiago@outlook.com.br>
2021-01-29 00:11:06 -0300

    Adding Domain layer

---
+++ mobile/iOSPokedex/iOSPokedexDomainTests/PokedexDomainTests.swift
@@ -0,0 +1,13 @@
+//
+//  iOSPokedexDomainTests.swift
+//  iOSPokedexDomainTests
+//
+//  Created by Thiago Oliveira on 29/01/21.
+//
+
+import XCTest
+@testable import iOSPokedexDomain
+
+class PokedexDomainTests: XCTestCase {
+
+}
