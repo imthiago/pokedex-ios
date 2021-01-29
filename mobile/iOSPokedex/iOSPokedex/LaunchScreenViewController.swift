0233a70016e6cbb8a136a1f8e061d725bb0f093c
Thiago Oliveira <thiago@outlook.com.br>
2021-01-28 23:57:26 -0300

    Adding Lottie pod and animation in launch screen

--- mobile/iOSPokedex/iOSPokedex/LaunchScreenViewController.swift
+++ mobile/iOSPokedex/iOSPokedex/LaunchScreenViewController.swift
@@ -6,32 +6,48 @@
 //
 
 import UIKit
+import Lottie
 
 class LaunchScreenViewController: UIViewController {
 
     private let imageView: UIImageView = {
-        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 428, height: 926))
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 478, height: 976))
         imageView.image = UIImage(named: "splash")
         return imageView
     }()
+
+    private var animationView: AnimationView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         view.addSubview(imageView)
+        setupLottieAnimation()
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         imageView.center = view.center
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+        DispatchQueue.main.async {
             self.animate()
         }
     }
 
+    private func setupLottieAnimation() {
+        animationView = .init(name: "22892-pikachu")
+        animationView?.frame = CGRect(x: 0, y: 0, width: 299.6, height: 648.2)
+        animationView?.center = view.center
+        animationView?.contentMode = .scaleAspectFit
+        animationView?.loopMode = .loop
+        animationView?.animationSpeed = 1
+        view.addSubview(animationView!)
+        animationView?.play()
+    }
+
     private func animate() {
-        UIView.animate(withDuration: 1.5, animations: {
+        UIView.animate(withDuration: 2, delay: 1.5, animations: {
             self.imageView.alpha = 0
+            self.animationView?.alpha = 0
         }, completion: { done in
             if done {
                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
