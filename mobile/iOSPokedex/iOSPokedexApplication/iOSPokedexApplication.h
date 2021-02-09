8f4efc2c84a82e5e77688731427d809ae65d7404
Thiago Oliveira <thiago@outlook.com.br>
2021-02-09 20:33:16 -0300

    Adding initial Application layer

---
+++ mobile/iOSPokedex/iOSPokedexApplication/iOSPokedexApplication.h
@@ -0,0 +1,18 @@
+//
+//  iOSPokedexApplication.h
+//  iOSPokedexApplication
+//
+//  Created by Thiago Oliveira on 09/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSPokedexApplication.
+FOUNDATION_EXPORT double iOSPokedexApplicationVersionNumber;
+
+//! Project version string for iOSPokedexApplication.
+FOUNDATION_EXPORT const unsigned char iOSPokedexApplicationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSPokedexApplication/PublicHeader.h>
+
+
