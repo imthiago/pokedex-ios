ecf3173551439260ae1a53fce33cacafd8dabcc2
Thiago Oliveira <thiago@outlook.com.br>
2021-02-09 20:26:08 -0300

    Adding default domain layer

---
+++ mobile/iOSPokedex/iOSPokedexDomainTests/iOSPokedexDomainTests.swift
@@ -0,0 +1,16 @@
+//
+//  iOSPokedexDomainTests.swift
+//  iOSPokedexDomainTests
+//
+//  Created by Thiago Oliveira on 09/02/21.
+//
+
+import XCTest
+@testable import iOSPokedexDomain
+
+class iOSPokedexDomainTests: XCTestCase {
+
+    func test_() {
+
+    }
+}
