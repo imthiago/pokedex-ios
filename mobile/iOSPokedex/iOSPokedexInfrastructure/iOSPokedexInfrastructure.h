a9758f6b336cbc1b73139995519de83da80d6dd9
Thiago Oliveira <thiago@outlook.com.br>
2021-02-09 20:31:41 -0300

    Adding initial Infrastructure layer

---
+++ mobile/iOSPokedex/iOSPokedexInfrastructure/iOSPokedexInfrastructure.h
@@ -0,0 +1,18 @@
+//
+//  iOSPokedexInfrastructure.h
+//  iOSPokedexInfrastructure
+//
+//  Created by Thiago Oliveira on 09/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSPokedexInfrastructure.
+FOUNDATION_EXPORT double iOSPokedexInfrastructureVersionNumber;
+
+//! Project version string for iOSPokedexInfrastructure.
+FOUNDATION_EXPORT const unsigned char iOSPokedexInfrastructureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSPokedexInfrastructure/PublicHeader.h>
+
+
